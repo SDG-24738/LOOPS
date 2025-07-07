bc3b88b3acc65c20da5d0050ce883e04356a4f61
SDG-24738 <shashankvedantr@gmail.com>
2025-07-07 19:25:46 +0530

    Tuple operation

---
+++ Tuples/Tuple operations.py
@@ -0,0 +1,15 @@
+Tuple1 = (10, 3.14, True, "retribution")
+print(Tuple1)
+
+Tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(Tuple1)
+
+Tuple1 = Tuple1 + (9,)
+print(Tuple1)
+
+Tuple1 = (10, 30, 100, 1000, 100, 90, 657800, 750000, 100)
+print(Tuple1.count(100))
+
+Tuple1 = (9, 3, 4, 8, 1, 9, 2, 7, 6, 5)
+slice = Tuple1[3:5]
+print(slice)
