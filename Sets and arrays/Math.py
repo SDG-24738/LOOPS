a9e7f2938d7a214e0845f79b7c658293e5293dae
SDG-24738 <shashankvedantr@gmail.com>
2025-07-13 08:25:03 +0530

    ANGLES

---
+++ Sets and arrays/Math.py
@@ -0,0 +1,8 @@
+import math
+angle = float(input("Enter an angle in radians"))
+x = math.sin(angle)
+y = math.cos(angle)
+z = math.tan(angle)
+print("sin of", angle, "is equal to", x)
+print("cos of", angle, "is equal to", y)
+print("tan of", angle, "is equal to", z)
