8e95b77c0c7d0f584e10fca04258c540d0358650
SDG-24738 <shashankvedantr@gmail.com>
2025-07-13 19:35:00 +0530

    MULTI

---
+++ Sets and arrays/Tuples.py
@@ -0,0 +1,10 @@
+product = 1
+produuct2 = 1
+tuple1 = (4, 3, 2, 2, -1, 18)
+tuple2 = (2, 4, 8, 8, 3, 2, 9)
+for x in tuple1:
+    product = product * x
+for x in tuple2:
+    produuct2 = produuct2 * x
+print(product)
+print(produuct2)
