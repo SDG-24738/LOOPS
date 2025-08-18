e3c4938d6eefa0adb993b0cb45e0d79bdfcb6f78
SDG-24738 <shashankvedantr@gmail.com>
2025-08-18 20:59:56 +0530

    Vehicle polymorphism

---
+++ Projects/Vehiclepolymorphism.py
@@ -0,0 +1,15 @@
+class BMW():
+    def fuel_type(self):
+        print("The fuel type for BMW is petrol.")
+    def max_speed(self):
+        print("The max speed of the car is 300 Km/h.")
+class Ferrari():
+    def fuel_type():
+        print("The fuel type for ferrari is petrol.")
+    def max_speed(self):
+        print("The max speed of the car is 400Km/h.")
+obj_bmw = BMW()
+obj_ferrari = Ferrari()
+for car in (obj_bmw, obj_ferrari):
+    car.fuel_type()
+    car.max_speed()
